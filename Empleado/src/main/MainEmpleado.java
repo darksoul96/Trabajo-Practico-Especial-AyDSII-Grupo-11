52ff8dafd3e4392e391de7cae2ad68cf9c844492
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-05-05 18:24:45 -0300

    created main classes

---
+++ Empleado/src/main/MainEmpleado.java
@@ -0,0 +1,8 @@
+package main;
+
+public class MainEmpleado {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Server/src/main/MainServidor.java
@@ -0,0 +1,8 @@
+package main;
+
+public class MainServidor {
+
+	public static void main(String[] args) {
+		
+	}
+}

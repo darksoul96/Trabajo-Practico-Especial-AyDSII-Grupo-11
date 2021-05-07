bd852336d617d94049fd14e975506854b89aadb5
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-05-07 00:09:55 -0300

    Added VentanaEmpleado + VentanaPantalla

--- Pantalla/src/vista_pantalla/VentanaPantalla.java
+++ Pantalla/src/vista_pantalla/VentanaPantalla.java
@@ -1,5 +1,81 @@
 package vista_pantalla;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+
 public class VentanaPantalla {
 
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPantalla window = new VentanaPantalla();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaPantalla() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 700, 547);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 684, 72);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.DARK_GRAY);
+		panel_1.setBounds(0, 0, 339, 72);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DNI");
+		lblNewLabel.setForeground(new Color(248, 248, 255));
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+		lblNewLabel.setBounds(144, 11, 120, 36);
+		panel_1.add(lblNewLabel);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.DARK_GRAY);
+		panel_2.setBounds(345, 0, 339, 72);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Box Nro");
+		lblNewLabel_1.setForeground(new Color(248, 248, 255));
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 30));
+		lblNewLabel_1.setBounds(117, 11, 128, 38);
+		panel_2.add(lblNewLabel_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 83, 664, 414);
+		frame.getContentPane().add(textArea);
+	}
 }

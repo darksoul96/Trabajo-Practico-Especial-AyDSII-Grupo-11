8b7e2192590ec532e4d3109a9fc59dea3c7026e1
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2021-05-07 14:51:00 -0300

    Main Cliente lee de txt

--- Cliente/src/gestion_ingreso/Controller_Emisor_Cliente.java
+++ Cliente/src/gestion_ingreso/Controller_Emisor_Cliente.java
@@ -1,4 +1,5 @@
 package gestion_ingreso;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -11,40 +12,58 @@ import java.net.*;
 import interfaces.IVista;
 import vista_cliente.VentanaCliente;
 
-public class Controller_Emisor_Cliente implements ActionListener{
+public class Controller_Emisor_Cliente implements ActionListener {
 
-    private IVista view;
-    private String DNI;
+	private IVista view;
+	private String DNI;
+	private String ip;
+	private int port;
 
-    public Controller_Emisor_Cliente() {
-        super();
-        this.view = new VentanaCliente();
-        this.view.setActionListener(this);
-        this.view.setVisibleVentana();
-    }
+	public Controller_Emisor_Cliente(String ip, int port) {
+		this.ip = ip;
+		this.port = port;
+		this.view = new VentanaCliente();
+		this.view.setActionListener(this);
+		this.view.setVisibleVentana();
+	}
 
+	public String getIp() {
+		return ip;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String command=e.getActionCommand();
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
 
-        if (command.equalsIgnoreCase("INGRESAR")) {
-            this.DNI=(view.getTextoDNI());
-            try {
-                Socket socket = new Socket("localhost",5005);
-                //PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                //BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                OutputStream outputStream = socket.getOutputStream();
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-                objectOutputStream.writeObject(new Cliente(DNI));
-                socket.close();
+	public int getPort() {
+		return port;
+	}
 
-            }
-            catch(Exception e1) {
-                e1.printStackTrace();
-            }
-        }
+	public void setPort(int port) {
+		this.port = port;
+	}
 
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+
+		if (command.equalsIgnoreCase("INGRESAR")) {
+			this.DNI = (view.getTextoDNI());
+			try {
+				Socket socket = new Socket("localhost", 5005);
+				// PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				// BufferedReader in = new BufferedReader(new
+				// InputStreamReader(socket.getInputStream()));
+				OutputStream outputStream = socket.getOutputStream();
+				ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+				objectOutputStream.writeObject(new Cliente(DNI));
+				socket.close();
+
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+
+	}
 
 }
--- Cliente/src/main/MainCliente.java
+++ Cliente/src/main/MainCliente.java
@@ -1,13 +1,28 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import gestion_ingreso.Controller_Emisor_Cliente;
 
 public class MainCliente {
 
 	public static void main(String[] args) {
-		Controller_Emisor_Cliente emisor = new Controller_Emisor_Cliente();
-		
-		
+		try {
+			File myObj = new File("NetConfig.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		Controller_Emisor_Cliente emisor = new Controller_Emisor_Cliente("localhost",5005);
+
 	}
 
 }

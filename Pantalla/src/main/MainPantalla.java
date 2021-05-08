72a2b42509528a0fd4f52ce9587e417f91d47149
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-05-08 16:49:57 -0300

    Added changes: ControllerPantalla, IVista, MainPantalla, VentanaPantalla

--- Pantalla/src/main/MainPantalla.java
+++ Pantalla/src/main/MainPantalla.java
@@ -1,10 +1,14 @@
 package main;
 
+import java.awt.EventQueue;
+
+import vista_pantalla.VentanaPantalla;
+
 public class MainPantalla {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }

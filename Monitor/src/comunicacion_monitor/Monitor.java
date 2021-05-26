9b3e747abc130bccbe55ae664dae0762a488108d
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2021-05-26 15:53:51 -0300

    -Creada interface Disponibilidad y metodos - Creada clase Monitor

---
+++ Monitor/src/comunicacion_monitor/Monitor.java
@@ -0,0 +1,42 @@
+package comunicacion_monitor;
+
+import java.util.concurrent.locks.Lock;
+
+import interfaces.Disponibilidad;
+
+public class Monitor implements Disponibilidad {
+
+	private static Monitor instance = null;
+
+	private Monitor() {
+	}
+
+	public static Monitor getInstance() {
+		if (instance == null) {
+			Lock lock;
+			if (instance == null) {
+				instance = new Monitor();
+			}
+		}
+		return instance;
+	}
+
+	@Override
+	public void ping() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean primaryServerOnline() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean secondaryServerOnline() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Monitor/src/interfaces/Disponibilidad.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface Disponibilidad {
+
+	public void ping();
+	public boolean primaryServerOnline();
+	public boolean secondaryServerOnline();
+}

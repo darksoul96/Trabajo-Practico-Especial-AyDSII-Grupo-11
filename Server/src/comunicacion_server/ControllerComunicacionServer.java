5934cd24b4ef3edfd8dcf22897f758235783a3e4
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-05-28 20:46:55 -0300

    cambios varios

--- Server/src/comunicacion_server/ControllerComunicacionServer.java
+++ Server/src/comunicacion_server/ControllerComunicacionServer.java
@@ -1,12 +1,14 @@
 package comunicacion_server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -135,7 +137,7 @@ public class ControllerComunicacionServer implements ComunicacionServer {
 
 					Socket socket = new Socket("localhost", 5000);
 					Servidor.getInstance().setSecondary();
-					//ventanaServer.setSecundario();
+					ventanaServer.setSecundario();
 					System.out.println("Soy Secundario");
 					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 					out.println("connected");
@@ -147,12 +149,15 @@ public class ControllerComunicacionServer implements ComunicacionServer {
 						packageHandler.handle(backup);
 						System.out.println("Me llego msj del server");
 					}
-				} catch (Exception e1) {
+				} catch (ConnectException e1) {
 					e1.printStackTrace();
 					ventanaServer.setPrimario();
 					Servidor.getInstance().setPrimary();
 					recibir();
 					System.out.println("Soy Primario");
+				}
+				catch (Exception e2) {
+					
 				}
 			}
 		}.start();

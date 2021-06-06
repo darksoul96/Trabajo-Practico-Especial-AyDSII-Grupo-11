5aa6543b99fbcc82995bd41f94ba6676ab4dc6ad
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2021-06-06 20:57:11 -0300

    Principios de la persistencia y cambios en atributos de cliente

--- Cliente/src/comunicacion_ingreso/Cliente.java
+++ Cliente/src/comunicacion_ingreso/Cliente.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 public class Cliente implements Serializable{
 	String DNI;
 	String box;
+	String nombre;
+	int prioridad;
 	
 	public Cliente(String dNI) {
 		super();
@@ -23,6 +25,24 @@ public class Cliente implements Serializable{
 		return this.box;
 	}
 	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+	public int getPrioridad() {
+		return prioridad;
+	}
+
+	public void setPrioridad(int prioridad) {
+		this.prioridad = prioridad;
+	}
+
 	@Override
 	public String toString() {
 		return this.DNI;
---
+++ Server/src/persistencia/PersistidorXML.java
@@ -0,0 +1,44 @@
+package persistencia;
+
+import java.io.IOException;
+
+public class PersistidorXML implements IPersistencia  {
+
+	@Override
+	public void abrirInput(String nombre) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void abrirOutput(String nombre) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cerrarInput() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cerrarOutput() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void escribir(Object objeto) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object leer() throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}

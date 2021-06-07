440ab8e7a2c882ef54bac2ea33de8bfb3c95828e
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-06-07 14:37:19 -0300

    cambios en el persistidor

--- Server/src/persistencia/PersistidorXML.java
+++ Server/src/persistencia/PersistidorXML.java
@@ -1,5 +1,9 @@
 package persistencia;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import interfaces.IAccesoBaseDatos;
@@ -7,40 +11,50 @@ import interfaces.IPersistencia;
 
 public class PersistidorXML implements IPersistencia, IAccesoBaseDatos  {
 
+	private XMLEncoder xmlEncoder;
+	private XMLDecoder xmlDecoder;
+	private FileOutputStream fileoutput;
+	private FileInputStream fileinput;
+	
 	@Override
 	public void abrirInput(String nombre) throws IOException {
 		// TODO Auto-generated method stub
-		
+		fileinput = new FileInputStream(nombre);
+		xmlDecoder = new XMLDecoder(fileinput);
 	} 
 
 	@Override
 	public void abrirOutput(String nombre) throws IOException {
 		// TODO Auto-generated method stub
-		
+		fileoutput = new FileOutputStream(nombre);
+		xmlEncoder = new XMLEncoder(fileoutput);
 	}
 
 	@Override
 	public void cerrarInput() throws IOException {
 		// TODO Auto-generated method stub
-		
+		this.xmlDecoder.close();
 	}
 
 	@Override
 	public void cerrarOutput() throws IOException {
 		// TODO Auto-generated method stub
-		
+		this.xmlEncoder.close();
 	}
 
 	@Override
 	public void escribir(Object objeto) throws IOException {
 		// TODO Auto-generated method stub
-		
+		this.xmlEncoder.writeObject(objeto);
 	}
 
 	@Override
 	public Object leer() throws IOException, ClassNotFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		Object objeto = null;
+		if (xmlDecoder != null)
+			objeto = xmlDecoder.readObject();
+		return objeto;
 	}
 
 	

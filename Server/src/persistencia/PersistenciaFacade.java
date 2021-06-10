52077010bc213b54ba215fb072a8499180e47bc6
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2021-06-10 15:05:36 -0300

    Check Horario de atencion

--- Server/src/persistencia/PersistenciaFacade.java
+++ Server/src/persistencia/PersistenciaFacade.java
@@ -14,40 +14,24 @@ public class PersistenciaFacade implements IAccesoBaseDatos {
 
 	IPersistencia persistencia;
 	private ArrayList<Cliente> clientesEnDB = new ArrayList<Cliente>();
-	
+
 	public PersistenciaFacade() {
 		super();
 		this.persistencia = new PersistidorXML();
 	}
 
 	public void generaLista() {
-		/*
-		Cliente cliente1 = new Cliente();
-		cliente1.setNombre("Seba");
-		cliente1.setPrioridad(1);
-		cliente1.setDNI("38828813");
-		Cliente cliente2 = new Cliente();
-		cliente2.setNombre("Agus");
-		cliente2.setPrioridad(2);
-		cliente2.setDNI("39555671");
-		Cliente cliente3 = new Cliente();
-		cliente3.setNombre("Jorge");
-		cliente3.setPrioridad(3);
-		cliente3.setDNI("1");
-		*/
-		
-		
+
 		try {
 			persistencia.abrirOutput("clientes.xml");
 			this.llenarBaseDeDatos();
-			while(clientesEnDB.size()!=0) {
+			while (clientesEnDB.size() != 0) {
 				persistencia.escribir(clientesEnDB.remove(0));
 			}
 			persistencia.cerrarOutput();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Ta creando");
 
 	}
 
@@ -56,26 +40,26 @@ public class PersistenciaFacade implements IAccesoBaseDatos {
 		Cliente actual;
 		String lineaTotal, linea2 = "", nombre, apellido, dni;
 		int prioridad, index;
-		
+
 		try {
-			File archivo=new File("clientes.txt");
+			File archivo = new File("clientes.txt");
 			Scanner scan = new Scanner(archivo);
 			while (scan.hasNextLine()) {
-				lineaTotal=scan.nextLine();
-				prioridad=Integer.parseInt(lineaTotal.substring(0, 1));
-				linea2=lineaTotal.substring(2);
-				index=linea2.indexOf(' ');
-				nombre=linea2.substring(0, index);
-				linea2=linea2.substring(index+1);
-				index=linea2.indexOf(' ');
-				apellido=linea2.substring(0, index);
-				linea2=linea2.substring(index);
-				dni=linea2;
-				dni = dni.replaceAll("\\s","");
-				
-				actual=new Cliente();
+				lineaTotal = scan.nextLine();
+				prioridad = Integer.parseInt(lineaTotal.substring(0, 1));
+				linea2 = lineaTotal.substring(2);
+				index = linea2.indexOf(' ');
+				nombre = linea2.substring(0, index);
+				linea2 = linea2.substring(index + 1);
+				index = linea2.indexOf(' ');
+				apellido = linea2.substring(0, index);
+				linea2 = linea2.substring(index);
+				dni = linea2;
+				dni = dni.replaceAll("\\s", "");
+
+				actual = new Cliente();
 				actual.setDNI(dni);
-				actual.setNombre(nombre+" "+apellido);
+				actual.setNombre(nombre + " " + apellido);
 				actual.setPrioridad(prioridad);
 				clientesEnDB.add(actual);
 			}
@@ -84,13 +68,13 @@ public class PersistenciaFacade implements IAccesoBaseDatos {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 	@Override
 	public Cliente completaCliente(Cliente cliente) {
 		Cliente clienteActual = null;
+		String horaDeRegistro = cliente.getHorarioRegistro();
 		try {
 			persistencia.abrirInput("clientes.xml");
 			try {
@@ -102,6 +86,7 @@ public class PersistenciaFacade implements IAccesoBaseDatos {
 
 				try {
 					clienteActual = (Cliente) persistencia.leer();
+					clienteActual.setHorarioRegistro(horaDeRegistro);
 				} catch (IndexOutOfBoundsException e) {
 					clienteActual = null;
 					break;
